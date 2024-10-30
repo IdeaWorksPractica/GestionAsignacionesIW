e96ef020c18e21224ced11aaf65ac7c161cc2395
HaroruDa3 <harol.kock34@gmail.com>
2024-10-30 17:12:10 -0600

    xx
    xxx

--- src/home/ui/main/home.tsx
+++ src/home/ui/main/home.tsx
@@ -25,14 +25,13 @@ export const Home = () => {
     fetchUserData();
   }, []);
 
-  const logOut = async () =>{
-  try {
-    await logout();
-    navigate('/');
-  } catch (error) {
-    console.log(error);
-  }
-
+  const logOut = async () => {
+    try {
+      await logout();
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const showDrawer = () => {
@@ -63,17 +62,18 @@ export const Home = () => {
             <section className="sideBar">
               <div className="info-sideBar">
                 <img src="./logoIW.png" alt="logoIW" />
-                <p>{userData ? userData.nombre : "Nombre Usuario"}</p>{" "}
+                <p>{userData ? userData.nombre : "Nombre Usuario"}</p>
               </div>
               <div className="opt-section">
-                <div className="opt-container">
-                <p className="option">Gestion de áreas</p>
-                <p className="option">Asignaciones</p>
-                <p className="option">Usuarios</p>
-             
-                </div>
+                {(userRol === 'Jefe' || userRol === 'Admin') && (
+                  <div className="opt-container">
+                    <p className="option">Gestión de áreas</p>
+                    <p className="option">Asignaciones</p>
+                    <p className="option">Usuarios</p>
+                  </div>
+                )}
                 <div className="btn-logout-container">
-                    <p onClick={logOut} className="option">Cerrar sesión</p>
+                  <p onClick={logOut} className="option">Cerrar sesión</p>
                 </div>
               </div>
             </section>
@@ -116,42 +116,44 @@ export const Home = () => {
             <div className="sidebar-content">
               <div className="info-sideBar">
                 <section className="section-btn-drawer">
-                <button
-                  onClick={closeDrawer}
-                  className="btn btn-close-offcanvas"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon icon-tabler icon-tabler-x"
-                    width="32"
-                    height="32"
-                    viewBox="0 0 24 24"
-                    stroke-width="2.5"
-                    stroke="#ffffff"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                  <button
+                    onClick={closeDrawer}
+                    className="btn btn-close-offcanvas"
                   >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M18 6l-12 12" />
-                    <path d="M6 6l12 12" />
-                  </svg>
-                </button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon icon-tabler icon-tabler-x"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                      stroke-width="2.5"
+                      stroke="#ffffff"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M18 6l-12 12" />
+                      <path d="M6 6l12 12" />
+                    </svg>
+                  </button>
                 </section>
                 <section className="section-logo-drawer">
                   <img src="./logoIW.png" alt="logoIW" />
-                  <p>{userData ? userData.nombre : "Nombre Usuario"}</p>{" "}
-                </section> 
+                  <p>{userData ? userData.nombre : "Nombre Usuario"}</p>
+                </section>
               </div>
               <div className="opt-section">
-                <div className="opt-container">
-                <p className="option">Gestion de áreas</p>
-                <p className="option">Asignaciones</p>
-                <p className="option">Usuarios</p>
-             
-                </div>
+                {/* Renderiza las opciones en el Drawer si el rol es Jefe o Admin */}
+                {(userRol === 'Jefe' || userRol === 'Admin') && (
+                  <div className="opt-container">
+                    <p className="option">Gestión de áreas</p>
+                    <p className="option">Asignaciones</p>
+                    <p className="option">Usuarios</p>
+                  </div>
+                )}
                 <div className="btn-logout-container">
-                    <p onClick={logOut} className="option">Cerrar sesión</p>
+                  <p onClick={logOut} className="option">Cerrar sesión</p>
                 </div>
               </div>
             </div>
