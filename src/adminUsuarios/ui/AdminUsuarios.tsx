dae8302a4b6678e4b0c97573ea0af0fba8dd615c
HaroruDa3 <harol.kock34@gmail.com>
2024-10-31 00:08:23 -0600

    x
    x

--- src/adminUsuarios/ui/AdminUsuarios.tsx
+++ src/adminUsuarios/ui/AdminUsuarios.tsx
@@ -6,8 +6,43 @@ export const AdminUsuarios = () => {
       <section className='title-container'>
         <p>Usuarios</p>
       </section>
-      <section className='users-cards-container'>
+      <section className='cards-container'>
+        <div className='container-fluid'>
+          <div className='row'>
+             <div className='col-12 col-xl-4 col-md-4 col-sm-6'>
+                <div className='card-user'>
+                  <div className='title-cards'>
+                    <span>Pablo Chacon</span>
+                  </div>
+                  <div className='info-card-container'>
+                    <div className='mb-2'>
+                      <span>Correo:</span><br/>
+                      <span>pablo@chacon.com</span>
+                    </div>
+                    <div className='mb-2'>
+                      <span>Area:</span><br/>
+                      <span>Proyectos y desarrollo</span>
+                    </div>
+                    <div className='mb-2'>
+                      <span>Puesto:</span><br/>
+                      <span>Desarrollador</span>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div className='col-12 col-xl-4 col-md-4 col-sm-6'>
+                <div className='card-user'>
 
+                </div>
+            </div>
+            <div className='col-12 col-xl-4 col-md-4 col-sm-6'>
+                <div className='card-user'>
+
+                </div>
+            </div>
+
+          </div>
+        </div>
       </section>
     </main>
   )
