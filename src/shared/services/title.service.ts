948da4e00a44b6cd2da66d455d37a06a050c3161
HaroruDa3 <harol.kock34@gmail.com>
2024-11-18 00:03:41 -0600

    x
    x

--- src/shared/services/title.service.ts
+++ src/shared/services/title.service.ts
@@ -1,6 +1,10 @@
 export const getScreenWidthMinusOffset = (offset: number = 228): number => {
   if (typeof window !== "undefined") {
-    return Math.max(0, window.innerWidth - offset);
+    const widthTotal = window.innerWidth;
+    if(widthTotal <= 580){
+      return widthTotal;
+    }
+    return Math.max(0, widthTotal - offset);
   } else {
     console.warn(
       "Window is not defined. Are you running this on the server side?"
